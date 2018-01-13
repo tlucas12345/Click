d0ab64a1a3e284470f5129676e007fd5ff8a3882
donnaken15 <myminecraftfriend15@yahoo.com>
2018-01-13 14:24:07 -0500

    Safe wipe save but will also delete settings

--- safeReset.js
+++ safeReset.js
@@ -1,11 +1,1 @@
-for (i = 0; i < Game.UpgradesById.length; i++) { 
-    Game.UpgradesById[i].bought=false;
-}
-
-for (i = 0; i < Game.ObjectsById.length; i++) { 
-    Game.ObjectsById[i].amount=0;
-    Game.ObjectsById[i].sell();
-}
-
-Game.cookies=0;
-Game.cookiesEarned=0;
+Game.localStorageSet("","");location.reload();
