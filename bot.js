2a0bf1a18c9305caedd83793a69323f0e49aac26
donnaken15 <myminecraftfriend15@yahoo.com>
2018-01-13 14:45:46 -0500

    Cookie Clicker bot

---
+++ bot.js
@@ -0,0 +1,23 @@
+function bot() {
+    Game.ClickCookie();
+    Game.ClickTinyCookie();
+    Game.ClickProduct(0);
+    Game.ClickProduct(1);
+    Game.ClickProduct(2);
+    Game.ClickProduct(3);
+    Game.ClickProduct(4);
+    Game.ClickProduct(5);
+    Game.ClickProduct(6);
+    Game.ClickProduct(7);
+    Game.ClickProduct(8);
+    Game.ClickProduct(9);
+    Game.ClickProduct(10);
+    Game.ClickProduct(11);
+    Game.ClickProduct(12);
+    Game.ClickProduct(13);
+    Game.ClickProduct(14);
+    for (i = 0; i < Game.UpgradesById.length; i++) { 
+      Game.UpgradesById[i].buy();
+    }
+}
+setInterval(bot, 30);
