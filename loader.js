e3564992b03c8cc3b809444ff954bc1d49aeb211
donnaken15 <myminecraftfriend15@yahoo.com>
2018-01-15 16:51:47 -0500

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,32 @@
+function Cookiebot() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/Cookiebot.js");
+}
+
+function Cookiegod() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/Cookiegod.js");
+}
+
+function cookiePresser() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/cookiePresser.js");
+}
+
+function allAchievements() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/allAchievements.js");
+}
+
+function debugMode() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/debugMode.js");
+}
+
+function disableAutosave() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/disableAutosave.js");
+}
+
+function glitchyName() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/glitchyName.js");
+}
+
+function safeReset() {
+  Game.LoadMod("https://donnaken15.github.io/wesleyscookies/safeReset.js");
+}
+
